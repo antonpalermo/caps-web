ce2786dea56096ec67eec84b947a7846901b4e93
Anton Palermo <ANPBusinessmail@gmail.com>
2022-03-05 11:39:07 +0800

    feat: auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,7 +3,13 @@ import GoogleProvider from 'next-auth/providers/google'
 
 import { Provider } from 'next-auth/providers'
 import { NextApiRequest, NextApiResponse } from 'next'
-
+import { TypeORMLegacyAdapter } from '@next-auth/typeorm-legacy-adapter'
+import {
+	AccountEntity,
+	SessionEntity,
+	UserEntity,
+	VerificationTokenEntity
+} from '@next-auth/typeorm-legacy-adapter/dist/entities'
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	const providers: Provider[] = [
 		GoogleProvider({
@@ -13,7 +19,8 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	]
 
 	return await NextAuth(req, res, {
-		providers
+		providers,
+		adapter: TypeORMLegacyAdapter(process.env.DATABASE_CONNECTION || '')
 	})
 }
 
